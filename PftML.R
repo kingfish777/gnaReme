1f244f602ae5a0de63d1ff23191037a5d953ef11
Kingfish Threesevens <scott.malec@gmail.com>
2014-02-01 22:50:48 -0500

    Create PftML.R

---
+++ PftML.R
@@ -0,0 +1,11 @@
+library(XML)
+library(tm)
+library(RCurl)
+url <- "http://clover.slavic.pitt.edu/sam/propp/have_a_little_byte/magicgeese.xml"
+#url <- "http://www.maleclabs.com/Propp/Corpus.xml"
+tale <- xmlTreeParse(getURL(url), useInternal = T)
+tale
+initsit <- xmlValue(getNodeSet(tale, "//Corpus//Folktale//Move//Preparation//InitialSituation")[[1]])
+initsit
+return <- xmlValue(getNodeSet(tale, "//Corpus//Folktale//Move//Return")[[1]])
+return
