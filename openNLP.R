113ae89dba2f0ef98bcea6557936e1e7ed72f8bc
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-23 18:19:34 -0400

    Update openNLP.R

--- openNLP.R
+++ openNLP.R
@@ -1,15 +1,22 @@
 library(XML)
 library(tm)
 library(RCurl)
-url <- "http://clover.slavic.pitt.edu/sam/propp/have_a_little_byte/magicgeese.xml"
-#url <- "http://www.maleclabs.com/Propp/Corpus.xml"
+#url <- "http://clover.slavic.pitt.edu/sam/propp/have_a_little_byte/magicgeese.xml"
+setwd("/home/kingfish")
+url <- "Corpus_Rus.xml"
 tale <- xmlTreeParse(getURL(url), useInternal = T)
 tale
 initsit <- xmlValue(getNodeSet(tale, "//Corpus//Folktale//Move//Preparation//InitialSituation")[[1]])
 initsit
 return <- xmlValue(getNodeSet(tale, "//Corpus//Folktale//Move//Return")[[1]])
 return
-library(openNLP)
-# sentDetect(initsit, language = "en", model = NULL)
-y <- tagPOS(initsit, language = "en", model = NULL, tagdict = NULL)
-y
+
+Trans <- getNodeSet(tale, "//Corpus//Folktale//Move//Transfiguration")
+Trans
+
+Villainy <- getNodeSet(tale, "//Corpus//Folktale//Move//Villainy")
+Villainy
+
+Lack <- getNodeSet(tale, "//Corpus//Folktale//Move//Lack")
+Lack
+
