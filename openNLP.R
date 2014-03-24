962f331a2fa3651aa8f9161d80610330bcf9e96b
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-23 22:04:45 -0400

    Update openNLP.R

--- openNLP.R
+++ openNLP.R
@@ -1,6 +1,7 @@
 library(XML)
 library(tm)
 library(RCurl)
+library(RTextTools)
 #url <- "http://clover.slavic.pitt.edu/sam/propp/have_a_little_byte/magicgeese.xml"
 setwd("/home/kingfish")
 url <- "Corpus_Rus.xml"
@@ -72,10 +73,84 @@ for (i in 1:length(Villainy))
 for (i in 1:length(Lack)) 
  { writeLines(as.character(getChildrenStrings(Villainy[[i]])), paste("/home/kingfish/proppian_function_language_models/Lack/Lack", as.character(i), ".txt", sep=""))  } 
  
- 
-
 for (i in 2:length(Return)) 
  { writeLines(as.character(getChildrenStrings(Return[[i]])), paste("/home/kingfish/proppian_function_language_models/Return/Return", as.character(i), ".txt", sep=""))  } 
- 
- 
- 
+
+# and so on
+############################
+# CREATE LANGUAGE MODELS
+############################
+
+###################
+#VILLAINY LANGUAGE MODEL
+###################
+wd <- "/home/kingfish/proppian_function_language_models/Villainy"
+setwd(wd)
+text <- system.file("texts", "txt", package="tm")
+corpus <- Corpus(DirSource('.'))
+corpus <- tm_map(corpus, function(x) iconv(enc2utf8(x), sub = "byte"))
+corpus <- tm_map(corpus, removeWords, stopwords("SMART"))
+corpus <- tm_map(corpus, removeWords, c(stopwords("russian")))
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, tolower)
+corpus <- tm_map(corpus, stripWhitespace)
+####################
+#VILLAINY
+#dtm.villainy <- DocumentTermMatrix(corpus)
+###########################
+dtm.villainy <- create_matrix(cbind(as.vector(corpus)), language="russian", minDocFreq=1, maxDocFreq=Inf,
+              minWordLength=3, maxWordLength=Inf, ngramLength=3, originalMatrix=NULL,
+              removeNumbers=FALSE, removePunctuation=TRUE, removeSparseTerms=0,
+              removeStopwords=TRUE, stemWords=FALSE, stripWhitespace=TRUE, toLower=TRUE,
+              weighting=weightTf)
+#rowTotals <- apply(dtm.villainy, 1, sum) #Find the sum of words in each Document
+#dtm.villainy <- dtm.villainy[rowTotals> 0] #remove all docs without words
+# dtm <- removeSparseTerms(dtm, 0.99)
+lsa.villainy <- lsa(dtm.villainy, dims = dimcalc_share(share = .5))
+lsa_k.villainy <- lsa(dtm.villainy, dims = dimcalc_kaiser())
+plot(dtm.villainy)
+plot(dtm.villainy, corThreshold = 0.5)
+plot(dtm.villainy, corThreshold = 0.5, terms = findFreqTerms(dtm.villainy, 6, Inf))
+inspect(corpus)[[20]]
+summary(lsa.villainy)
+######
+
+###################
+#LACK LANGUAGE MODEL
+###################
+wd <- "/home/kingfish/proppian_function_language_models/Lack"
+setwd(wd)
+text <- system.file("texts", "txt", package="tm")
+corpus <- Corpus(DirSource('.'))
+corpus <- tm_map(corpus, function(x) iconv(enc2utf8(x), sub = "byte"))
+corpus <- tm_map(corpus, removeWords, stopwords("SMART"))
+corpus <- tm_map(corpus, removeWords, c(stopwords("english")))
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, tolower)
+corpus <- tm_map(corpus, stripWhitespace)
+####################
+#VILLAINY
+#dtm.villainy <- DocumentTermMatrix(corpus)
+###########################
+dtm.lack <- create_matrix(cbind(as.vector(corpus)), language="english", minDocFreq=1, maxDocFreq=Inf,
+              minWordLength=3, maxWordLength=Inf, ngramLength=3, originalMatrix=NULL,
+              removeNumbers=FALSE, removePunctuation=TRUE, removeSparseTerms=0,
+              removeStopwords=TRUE, stemWords=FALSE, stripWhitespace=TRUE, toLower=TRUE,
+              weighting=weightTf)
+rowTotals <- apply(dtm.lack, 1, sum) #Find the sum of words in each Document
+dtm.lack <- dtm.lack[rowTotals> 0] #remove all docs without words
+# dtm <- removeSparseTerms(dtm, 0.99)
+lsa.lack <- lsa(dtm.lack, dims = dimcalc_share(share = .5))
+lsa_k.lack <- lsa(dtm.lack, dims = dimcalc_kaiser())
+plot(dtm.lack)
+plot(dtm.lack, corThreshold = 0.5)
+plot(dtm.lack, corThreshold = 0.5, terms = findFreqTerms(dtm.lack, 6, Inf))
+inspect(corpus)[[20]]
+summary(lsa.lack)
+######
+tm::Zipf_plot(dtm.villainy)
+tm::Heaps_plot(dtm.villainy) 
+
+
