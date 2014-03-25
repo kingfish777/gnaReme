09663f32da2538a27f45971a439f62e965b4ddcd
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-24 21:24:47 -0400

    Update mainAutoPropp.R

--- mainAutoPropp.R
+++ mainAutoPropp.R
@@ -1,10 +1,24 @@
+#CREATE LANGUAGE MODELS of PROPPIAN FUNCTIONS
 source("heavyLifterRUS.R")
 source("createTextSegmentsRUS.R")
 
 
+#NOW RUN EXPERIMENTS
 
+#EXPERIMENT I
+# random snippets
+#      ... DNA
+#      ... java code
+#      ... bureaucratic/legal  language
+#      ... Russian magic tales
 
+# EXPERIMENT II
+# functions where they don't belong
+#  -> villainy with return, acquisition of magical agent with wedding, liquidation of lack with lack
+#  -> randomized samples vs. 'held out' data from markup
 
+#EXPERIMENT II
+# texttiling snippets of Russian magic tales
 
 
 
