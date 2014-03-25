2842deb7570b45aee726e51713e2564eae605120
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-24 20:28:40 -0400

    Create createStructure.sh

---
+++ createStructure.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "creating top level folder"
+mkdir proppian_function_language_models
+cd proppian_function_language_models
+echo "creating sub folders"
+mkdir InitialSituation
+mkdir Lack
+mkdir Villainy
+mkdir DonorFunction
+mkdir HeroReaction
+mkdir AcquisitionOfMagicalAgent
+mkdir Transfiguration
+mkdir Return
+mkdir Wedding
+mkdir Epilogue
+echo "creating experiment folders"
+mkdir TextTiling
+mkdir RandomTexts
