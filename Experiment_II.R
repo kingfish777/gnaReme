06b6278629072497e13e9c9c035b4a40fba8f6d5
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-24 21:50:33 -0400

    Create Experiment_II.R

---
+++ Experiment_II.R
@@ -0,0 +1,21 @@
+##################################################################
+# EXPERIMENT II
+# functions where they don't belong
+#  -> villainy with return, acquisition of magical agent with wedding, liquidation of lack with lack
+#  -> randomized samples vs. 'held out' data from markup
+#
+#  what results will look like:
+#
+#             VillainySample1 VillainySample2 VillainySample3 etc.
+# VillainyLM     .8         
+# LackLM         .3
+# ...
+# WeddingLM   0
+# EpilogueLM   0
+#
+#  ... do the same with held out LackSamples ... WeddingSamples, EpilogueSamples         
+#
+# PURPOSE: show how language model from PftML can be useful even
+#               even if it is imperfect, rough-hewn
+#
+##################################################################
