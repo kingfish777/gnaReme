9d5a12a77e2c3e977d5af1cb91eee399663d7ac1
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-24 21:49:30 -0400

    Create Experiment_I.R

---
+++ Experiment_I.R
@@ -0,0 +1,12 @@
+###################################################################
+#EXPERIMENT I
+# random snippets
+#      ... DNA
+#      ... java code
+#      ... bureaucratic/legal  language
+#      ... Russian magic tales
+#
+# PURPOSE: demonstrate concepts like perplexity, entropy, ground work in
+#               classic information theory (Claude Shannon)
+#
+###################################################################
